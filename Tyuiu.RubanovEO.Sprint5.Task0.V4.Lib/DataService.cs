6b4e19d7af22c7d3e8fddd4e67a0aabb40fac16d
ЧЕЛОВЕК ЧЕЛОВЕК <olegoviz.2006@gmail.com>
2024-11-16 22:35:44 +0500

    Task0

--- Tyuiu.RubanovEO.Sprint5.Task0.V4.Lib/DataService.cs
+++ Tyuiu.RubanovEO.Sprint5.Task0.V4.Lib/DataService.cs
@@ -7,7 +7,7 @@ namespace Tyuiu.RubanovEO.Sprint5.Task0.V4.Lib
     {
         public string SaveToFileTextData(int x)
         {
-            string filePath = @"C:\Users\olego\source\repos\Tyuiu.RubanovEO.Sprint5\OutPutFileTask0.txt";
+            string filePath = "OutPutFileTask0.txt";
             File.Delete(filePath);
             double ans = Math.Round(2d * Math.Pow(x,3) + (1d/2d)*Math.Pow(x,2) - 3.5d * x + 2d, 3);
             File.AppendAllText(filePath, ans.ToString());
