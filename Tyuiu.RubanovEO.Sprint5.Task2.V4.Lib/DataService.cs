2f197c329cc01fe8afddd374a3833085982c35a7
ЧЕЛОВЕК ЧЕЛОВЕК <olegoviz.2006@gmail.com>
2024-11-18 18:08:15 +0500

    task2

--- Tyuiu.RubanovEO.Sprint5.Task2.V4.Lib/DataService.cs
+++ Tyuiu.RubanovEO.Sprint5.Task2.V4.Lib/DataService.cs
@@ -8,6 +8,8 @@ namespace Tyuiu.RubanovEO.Sprint5.Task2.V4.Lib
         public string SaveToFileTextData(int[,] matrix)
         {
             string filePath = @"C:\Users\olego\source\repos\Tyuiu.RubanovEO.Sprint5\OutPutFileTask2.csv";
+            string ans = "";
+            File.Delete(filePath);
             for (int i = 0; i < matrix.GetLength(0); i++)
             {
                 for (int j = 0; j < matrix.GetLength(1); j++)
@@ -20,10 +22,22 @@ namespace Tyuiu.RubanovEO.Sprint5.Task2.V4.Lib
                     {
                         matrix[i, j] = 0;
                     }
-                    try{ File.WriteAllText(filePath, Convert.ToString(matrix[i, j])); }catch (Exception ex) { }
+                    if (j == 2 && i != 2)
+                    {
+                        ans += $"{matrix[i, j]}\n";
+                    }
+                    else if (i == 2 && j == 2)
+                    {
+                        ans += $"{matrix[i, j]}";
+                    }
+                    else
+                    {
+                        ans += $"{matrix[i, j]};";
+                    }
                 }
             }
-            return filePath;
+            try{ File.WriteAllText(filePath, ans); }catch (Exception ex) { }
+            return ans;
         }
     }
 }
