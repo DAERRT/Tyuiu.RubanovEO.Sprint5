3ad08322449d91633a38d7cb1a7968344e2cb51b
ЧЕЛОВЕК ЧЕЛОВЕК <olegoviz.2006@gmail.com>
2024-11-18 20:04:03 +0500

    task4

---
+++ Tyuiu.RubanovEO.Sprint5.Task4.V11.Lib/DataService.cs
@@ -0,0 +1,15 @@
+﻿
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.RubanovEO.Sprint5.Task4.V11.Lib
+{
+    public class DataService : ISprint5Task4V11
+    {
+        public double LoadFromDataFile(string path)
+        {
+            string filePath = "InPutDataFileTask4V11.txt";
+            double x = double.Parse(File.ReadAllText(path));
+            return Math.Round(Math.Sin(x) + (Math.Pow(x,2)/2),3);
+        }
+    }
+}
---
+++ Tyuiu.RubanovEO.Sprint5.Task4.V11.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+namespace Tyuiu.RubanovEO.Sprint5.Task4.V11.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
