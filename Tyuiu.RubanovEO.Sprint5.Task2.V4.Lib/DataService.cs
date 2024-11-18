084b5df4fc40d04afff9e75422f471c69b5c5144
ЧЕЛОВЕК ЧЕЛОВЕК <olegoviz.2006@gmail.com>
2024-11-18 17:57:23 +0500

    task2

---
+++ Tyuiu.RubanovEO.Sprint5.Task2.V4.Lib/DataService.cs
@@ -0,0 +1,29 @@
+﻿
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.RubanovEO.Sprint5.Task2.V4.Lib
+{
+    public class DataService : ISprint5Task2V4
+    {
+        public string SaveToFileTextData(int[,] matrix)
+        {
+            string filePath = @"C:\Users\olego\source\repos\Tyuiu.RubanovEO.Sprint5\OutPutFileTask2.csv";
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i,j] > 0)
+                    {
+                        matrix[i, j] = 1;
+                    }
+                    else
+                    {
+                        matrix[i, j] = 0;
+                    }
+                    File.WriteAllText(filePath, Convert.ToString(matrix[i,j]));
+                }
+            }
+            return filePath;
+        }
+    }
+}
---
+++ Tyuiu.RubanovEO.Sprint5.Task2.V4/Program.cs
@@ -0,0 +1,41 @@
+﻿using Tyuiu.RubanovEO.Sprint5.Task2.V4.Lib;
+
+namespace Tyuiu.RubanovEO.Sprint5.Task2.V4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5| Выполнил: Рубанов Е. О. | ПКТБ-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Класс File. Запись данных в текстовый файл.                       *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #4                                                              *");
+            Console.WriteLine("* Выполнил: Рубанов Егор Олегович | ПКТБ-24-1                             *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дано выражение, вычислить его значение при x = 2, результат сохранить в *");
+            Console.WriteLine("* текстовый файл OutPutFileTask0.txt и вывести на консоль. Округлить до   *");
+            Console.WriteLine("* трёх знаков после запятой.                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int[,] arr = new int[3, 3];
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.WriteLine($"Ведите {j+1}-ый элемент {i+1}-ой строки");
+                    arr[i,j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.SaveToFileTextData(arr));
+            Console.ReadKey();
+        }
+    }
+}
