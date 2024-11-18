e23fe6f19d91bbfa656407ab37d213a3b098af36
ЧЕЛОВЕК ЧЕЛОВЕК <olegoviz.2006@gmail.com>
2024-11-18 18:45:36 +0500

    task3

---
+++ Tyuiu.RubanovEO.Sprint5.Task3.V4.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.RubanovEO.Sprint5.Task3.V4.Lib
+{
+    public class DataService : ISprint5Task3V4
+    {
+        public string SaveToFileTextData(int x)
+        {
+            string filePath = @"C:\Users\olego\source\repos\Tyuiu.RubanovEO.Sprint5\OutPutFileTask3.bin";
+            string ans = Convert.ToString(Math.Round(Math.Log((x+1d)/(x+2d)), 3));
+            try{using (BinaryWriter writer = new BinaryWriter(File.Open(filePath, FileMode.Append)))
+                {
+                    writer.Write(ans);
+                }
+            }catch (Exception ex) {}
+            return ans;
+        }
+    }
+}
---
+++ Tyuiu.RubanovEO.Sprint5.Task3.V4.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+namespace Tyuiu.RubanovEO.Sprint5.Task3.V4.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
