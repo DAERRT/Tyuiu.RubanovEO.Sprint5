d6bc65fbce09d44b421f05c64f37ee0f86efef49
ЧЕЛОВЕК ЧЕЛОВЕК <olegoviz.2006@gmail.com>
2024-11-19 23:34:16 +0500

    task3

--- Tyuiu.RubanovEO.Sprint5.Task3.V4.Lib/DataService.cs
+++ Tyuiu.RubanovEO.Sprint5.Task3.V4.Lib/DataService.cs
@@ -7,16 +7,13 @@ namespace Tyuiu.RubanovEO.Sprint5.Task3.V4.Lib
     {
         public string SaveToFileTextData(int x)
         {
-            string filePath = @"C:\Users\olego\source\repos\Tyuiu.RubanovEO.Sprint5\OutPutFileTask3.bin";
+            string filePath = @"OutPutFileTask3.bin";
             double ans = Math.Round(Math.Log((x+1d)/(x+2d)), 3);
-            string ans1;
-            try{using (BinaryWriter writer = new BinaryWriter(File.Open(filePath, FileMode.Append)))
-                {
-                    writer.Write(BitConverter.GetBytes(ans));
-                }
-            }catch (Exception ex) {}
-            //return Convert.ToString(ans);
-            return "JQaBlUOLzL8=";
+            using (BinaryWriter writer = new BinaryWriter(File.Open(filePath, FileMode.Append)))
+            {
+                writer.Write(BitConverter.GetBytes(ans));
+            }
+            return File.ReadAllText(filePath);
         }
     }
 }
