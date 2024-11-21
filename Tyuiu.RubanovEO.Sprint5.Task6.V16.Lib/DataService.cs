8e71d17e1c86db74fdd0bcb14e645f20ec6091b3
ЧЕЛОВЕК ЧЕЛОВЕК <olegoviz.2006@gmail.com>
2024-11-21 17:40:30 +0500

    task6

---
+++ Tyuiu.RubanovEO.Sprint5.Task6.V16.Lib/DataService.cs
@@ -0,0 +1,13 @@
+﻿
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.RubanovEO.Sprint5.Task6.V16.Lib
+{
+    public class DataService : ISprint5Task6V16
+    {
+        public int LoadFromDataFile(string path)
+        {
+            return 0;
+        }
+    }
+}
---
+++ Tyuiu.RubanovEO.Sprint5.Task6.V16.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+namespace Tyuiu.RubanovEO.Sprint5.Task6.V16.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
